3652f0dc2a07882ad155ec4c331716c2bcdef4a0
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-05 11:04:20 +0530

    Axios data fetching methods

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -7,6 +7,20 @@ class EmployeeService {
        console.log(data);
       return axios.post(`${this.baseUrl}/create`,data);
     }
-    
+    findAllEmployee() {
+      return axios.get(`${this.baseUrl}/findAllEmployee`);
+    }
+    deleteEmployee = (id) => {
+      return axios.delete(`${this.baseUrl}/deleteEmcreateployee/${id}`);
+    };
+    updateEmployee = (id, data) => {
+      console.log(id);
+      return axios.put(`${this.baseUrl}/updateEmployee/${id}`, data);
+    };
+    findEmployee = (id) => {
+      console.log(id);
+      return axios.get(`${this.baseUrl}/findEmployee/${id}`);
+      
+    };
   }
   export default new EmployeeService();
