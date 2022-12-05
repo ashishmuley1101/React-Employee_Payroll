202f155d88ec4b82a6b092d85b157feb30f7926b
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-12-06 00:53:18 +0530

    UC 4 Posting Emp Data to DB using Axios Method

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // Use Case 3 creating the Service class importing the Axios Lib.
+//  Use Case 4 CreateEmp method for storing back to Backend DB using Axios method.
 
 class EmployeeService {
     baseUrl = "http://localhost:8080";
